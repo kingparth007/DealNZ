9c65098a19fde4c47eb1c579fe888aaa4c2ca7ac
dilroop singh <dilroop.singh@gmail.com>
2017-11-23 16:34:17 +1300

    Added Deal Box

---
+++ DealsNZ/Controllers/AllDealsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealsNZ.Controllers
+{
+    public class AllDealsController : Controller
+    {
+        // GET: AllDeals
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
