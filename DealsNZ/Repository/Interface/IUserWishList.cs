53c1233a1c8cb5505439350b918b1688cc7167b9
Parth Rajeshbhai Patel <tiz00005hs@aspire2student.ac.nz>
2018-02-07 09:46:34 +1300

    WishList Check

--- DealsNZ/Repository/Interface/IUserWishList.cs
+++ DealsNZ/Repository/Interface/IUserWishList.cs
@@ -11,5 +11,6 @@ namespace DealsNZ.Repository.Interface
     interface IUserWishList: _IRepositoryList<WishList>
     {
         IEnumerable<DealsModels.DealViewModel> ViewWishListByUser(int UserID);
+        bool wishlistCheck(int DealId, int UserID);
     }
 }
